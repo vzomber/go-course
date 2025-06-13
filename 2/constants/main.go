1823e8ae6820813d70f2b7b6237820041f3d662c
vzomber <34099961+vzomber@users.noreply.github.com>
2025-06-13 14:18:55 +0300

    added 2 chapters of exercises

---
+++ 2/constants/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	const premiumPlanName = "PremiumPlan"
+	const basicPlanName = "Basic Plan"
+
+	fmt.Println("plan:", premiumPlanName)
+	fmt.Println("plan:", basicPlanName)
+}
---
+++ 2/formatting_strings/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name = "Saul Goodman"
+	const openRate = 30.5
+
+	msg := fmt.Sprintf("Hi %s, your open rate is %v percent", name, openRate)
+	// ?
+
+	// don't edit below this line
+
+	fmt.Println(msg)
+}
